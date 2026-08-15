30573bbcd469eb28d073b5e41b53837cfe9c8801
Noor-ul-Ain <noor.official.mm@gmail.com>
2026-08-14 21:55:12 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,88 @@
+/*....Alerts (pdf-1)....
+
+...Q1...
+alert("Welcome Sir")
+...Q2...
+alert("Error! Please enter a valid password.")
+...Q3...
+alert("Welcome to JS Land...\nHappy Coding!")
+...Q4...
+alert("Welcome to JS Land....")
+alert("Happy Coding!\nPrevent this page from creating additional dialogs.")
+...Q5...
+alert("Hello... I can run JS through my web browser's console")*/
+
+
+
+/*....Variables for Strings (pdf-2)....
+
+...Q1...
+var username;   
+...Q2...
+var myname="Noor ul ain";
+...Q3...
+var message="Hello World";
+alert(message);
+...Q4...
+var myname="John doe";
+alert(myname);
+var age="15 years old";
+alert(age);
+var course="Certified Mobile Application Development";
+alert(course);
+...Q5...
+var pizza="PIZZA\nPIZZ\nPIZ\nPI\nP";
+alert(pizza);
+...Q6...
+var email="noorulain@gmail.com";
+alert("My email address is " + email);
+...Q7...
+var book="A smarter way to learn JavaScript";
+alert("I am trying to learn from the book " + book);
+...Q8...
+var str="Yah! I can write HTML content through JavaScript";
+document.writeln(str);
+...Q9...
+var design="▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+alert(design);
+document.writeln(design);*/
+
+
+
+/*....Variables for Numbers (pdf-3)....
+
+...Q1...
+var age=18;
+alert("I am " + age + " years old");
+...Q2...
+var n=10;
+alert("You have visited this site " + n + " times");
+...Q3...
+var birthYear=2007;
+document.writeln("My birth year is " + birthYear + "<br>" + "Data type of my declared variable is " + typeof(birthYear));
+...Q4...
+var visitorName="Noor ul ain";
+var productTitle="T-shirt(s)";
+var quantity=5;
+document.writeln(visitorName + " ordered " + quantity + " " + productTitle + " on XYZ Clothing store");*/
+
+
+/*....Variables Names: Legal & Illegal (pdf-4)....
+
+...Q1....
+var message;
+var marks_total;
+var firstName;
+...Q2....
+var 1stName; (illegal)
+var my-name; (illegal)
+var @home; (illegal)
+var first name; (illegal)
+var _lastName; (legal)
+var $myName; (legal)
+var myName123; (legal)
+var myName_123; (legal)
+var MyName; (legal)
+...Q3...
+document.writeln("<h1>Rules for naming JS variables</h1>" + "<br>" + "Variable names can only contain letters, numbers, $, and _. For example $my_1stVariable" + "<br>" + "Variables must begin with a letter, $ or _. For example $name, _name or name" + "<br>" + "Variable names are case sensitive" + "<br>" + "Variable names should not be JS ");*/
+
