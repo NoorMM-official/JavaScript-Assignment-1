ae77c50f908730076f8f2188b81413b1f4c9c4e8
Noor-ul-Ain <noor.official.mm@gmail.com>
2026-08-14 22:43:42 -0700

    Refactor alerts and variable declarations to use let

--- app.js
+++ app.js
@@ -17,33 +17,33 @@ alert("Hello... I can run JS through my web browser's console")*/
 /*....Variables for Strings (pdf-2)....
 
 ...Q1...
-var username;   
+let username;   
 ...Q2...
-var myname="Noor ul ain";
+let myname="Noor ul ain";
 ...Q3...
-var message="Hello World";
+let message="Hello World";
 alert(message);
 ...Q4...
-var myname="John doe";
+let myname="John doe";
 alert(myname);
-var age="15 years old";
+let age="15 years old";
 alert(age);
-var course="Certified Mobile Application Development";
+let course="Certified Mobile Application Development";
 alert(course);
 ...Q5...
-var pizza="PIZZA\nPIZZ\nPIZ\nPI\nP";
+let pizza="PIZZA\nPIZZ\nPIZ\nPI\nP";
 alert(pizza);
 ...Q6...
-var email="noorulain@gmail.com";
+let email="noorulain@gmail.com";
 alert("My email address is " + email);
 ...Q7...
-var book="A smarter way to learn JavaScript";
+let book="A smarter way to learn JavaScript";
 alert("I am trying to learn from the book " + book);
 ...Q8...
-var str="Yah! I can write HTML content through JavaScript";
+let str="Yah! I can write HTML content through JavaScript";
 document.writeln(str);
 ...Q9...
-var design="▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+let design="▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
 alert(design);
 document.writeln(design);*/
 
@@ -52,37 +52,37 @@ document.writeln(design);*/
 /*....Variables for Numbers (pdf-3)....
 
 ...Q1...
-var age=18;
+let age=18;
 alert("I am " + age + " years old");
 ...Q2...
-var n=10;
+let n=10;
 alert("You have visited this site " + n + " times");
 ...Q3...
-var birthYear=2007;
+let birthYear=2007;
 document.writeln("My birth year is " + birthYear + "<br>" + "Data type of my declared variable is " + typeof(birthYear));
 ...Q4...
-var visitorName="Noor ul ain";
-var productTitle="T-shirt(s)";
-var quantity=5;
+let visitorName="Noor ul ain";
+let productTitle="T-shirt(s)";
+let quantity=5;
 document.writeln(visitorName + " ordered " + quantity + " " + productTitle + " on XYZ Clothing store");*/
 
 
 /*....Variables Names: Legal & Illegal (pdf-4)....
 
 ...Q1....
-var message;
-var marks_total;
-var firstName;
+let message;
+let marks_total;
+let firstName;
 ...Q2....
-var 1stName; (illegal)
-var my-name; (illegal)
-var @home; (illegal)
-var first name; (illegal)
-var _lastName; (legal)
-var $myName; (legal)
-var myName123; (legal)
-var myName_123; (legal)
-var MyName; (legal)
+let 1stName; (illegal)
+let my-name; (illegal)
+let @home; (illegal)
+let first name; (illegal)
+let _lastName; (legal)
+let $myName; (legal)
+let myName123; (legal)
+let myName_123; (legal)
+let MyName; (legal)
 ...Q3...
 document.writeln("<h1>Rules for naming JS variables</h1>" + "<br>" + "Variable names can only contain letters, numbers, $, and _. For example $my_1stVariable" + "<br>" + "Variables must begin with a letter, $ or _. For example $name, _name or name" + "<br>" + "Variable names are case sensitive" + "<br>" + "Variable names should not be JS ");*/
 
